7f3d0afcbcc442609e0ffc13fc5b7d5c5a0691bb
maendros <Valitsianius@hotmail.com>
2017-05-04 20:23:20 +0300

    contact

--- contact.php
+++ contact.php
@@ -1,13 +1,106 @@
 
-<?php
-/**
- * This example shows how to handle a simple contact form.
- */
-$msg = '';
+<!DOCTYPE html>
+<?php 
+
+require 'PHPMailerAutoload.php';
+
+session_start();
+
+include("functions/functions.php"); 
+include("header.php");?>
+
+		<main >
+
+		
+<div class="jumbotron jumbotron-sm">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-lg-12">
+                <h1 class="h1">
+                   <small> Επικοινωνείστε μαζί  μας </small></h1>
+                   <?php if (!empty($msg)) {
+    echo "<h2>$msg</h2>";
+} ?>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="well well-sm">
+                <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="name">
+                                Όνομα</label>
+                            <input type="text" class="form-control" id="name" placeholder="Συμπληρώστε το Όνομα σας" required="required" name="name" />
+                        </div>
+                        <div class="form-group">
+                            <label for="email">
+                                Email </label>
+                            <div class="input-group">
+                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+                                </span>
+                                <input type="email" class="form-control" id="email" placeholder="Συμπληρώστε το email σας" required="required" name="email" /></div>
+                        </div>
+               
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="name">
+                                Μήνυμα</label>
+                            <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
+                                placeholder="Μήνυμα" ></textarea>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-primary pull-right" id="btnContactUs" name="send">
+                            Αποστολή</button>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>
+        
+    </div>
+</div>
+
+			
+	   <?php include("footer.php"); ?>
+<?php 
+  /* if (isset($_POST['send'])) {
+        $name=$_POST['name'];
+    $sender_email=$_POST['email'];
+    $msg=$_POST['message'];
+    $support_email="studentcomme@gmail.com";// send all items to that email
+            
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            $headers .= "From: <$sender_email>" . "\r\n";
+            
+            $subject = "support";
+            
+            $message =$msg;
+            
+            mail($support_email,$subject,$message,$headers);
+
+                    echo "<script>alert('Tο email σας εστάλη')</script>";
+
+
+
+    echo "<script>window.open('index.php','_self')</script>"; 
+        
+    } */
+
+
+
+    $msg = '';
 //Don't run this unless we're handling a form submission
-if (array_key_exists('email', $_POST)) {
-    date_default_timezone_set('Etc/UTC');
-    require '../PHPMailerAutoload.php';
+if (isset($_POST['send'])) {
+   
+   
     //Create a new PHPMailer instance
     $mail = new PHPMailer;
     //Tell PHPMailer to use SMTP - requires a local mail server
@@ -48,27 +141,9 @@ EOT;
         $msg = 'Invalid email address, message ignored.';
     }
 }
-session_start();
 
-include("functions/functions.php"); 
-include("header.php");
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Contact form</title>
-</head>
-<body>
-<h1>Contact us</h1>
-<?php if (!empty($msg)) {
-    echo "<h2>$msg</h2>";
-} ?>
-<form method="POST">
-    <label for="name">Name: <input type="text" name="name" id="name"></label><br>
-    <label for="email">Email address: <input type="email" name="email" id="email"></label><br>
-    <label for="message">Message: <textarea name="message" id="message" rows="8" cols="20"></textarea></label><br>
-    <input type="submit" value="Send">
-</form>
-</body>
-</html>
+
+
+ ?>
+
+
